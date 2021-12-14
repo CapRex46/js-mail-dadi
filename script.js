611b53533853d52419c05124389c306185a4b6a2
agres <ale.agresta46@gmail.com>
2021-12-14 09:55:57 +0100

    mail fixed

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const mail = ['test1@mail.com', 'test2@mail.com', 'test3@mail.com', 'test4@mail.
 let mailcheck = false;
 
 
-let userEmail = prompt('Dimmi la tua email');
+const userEmail = prompt('Dimmi la tua email');
 
 
 for (let i = 0; i < mail.length; i++) {
-    if (userEmail.toLowerCase === mail[i].toLowerCase) {
+    if (mail[i].toLocaleLowerCase() === userEmail.toLocaleLowerCase()) {
         mailcheck = true;
     }
 }
